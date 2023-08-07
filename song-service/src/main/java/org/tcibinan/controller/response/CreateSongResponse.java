b23c9324c4678300492c5240bce854fe3ce91202
Andrey Tsibin <andrei_tcibin@epam.com>
2023-08-07 21:12:23 +0300

    Clean up song service

--- song-service/src/main/java/org/tcibinan/controller/response/CreateSongResponse.java
+++ song-service/src/main/java/org/tcibinan/controller/response/CreateSongResponse.java
@@ -3,14 +3,5 @@ package org.tcibinan.controller.response;
 import io.micronaut.serde.annotation.Serdeable;
 
 @Serdeable
-public class CreateSongResponse {
-    private final Long id;
-
-    public CreateSongResponse(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
+public record CreateSongResponse(Long id) {
 }
